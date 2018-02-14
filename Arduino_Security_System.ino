63842d5a75721161c51a1e91532991149899e997
TheMasonX <norrtec@yahoo.com>
2018-02-13 23:31:44 -0600

    Added a key press tone

--- Arduino_Security_System.ino
+++ Arduino_Security_System.ino
@@ -21,29 +21,64 @@ const int pinLength = 4; //PIN Length (4 char)
 char pin[pinLength] = {
   '1', '2', '3', '4'
 };
-char enteredPin[pinLength] = {
-  '0', '0', '0', '0'
-};
+char enteredPin[pinLength];
 
 const char commandChar = '*';
 const int commandLength = 2; //Command Sequence Length (2 char)
 const char changePinCommand[commandLength] = {
   '2', '3'
 };
-char enteredCommand[commandLength] = {
-  '0', '0'
-};
+char enteredCommand[commandLength];
 const int commandTimeout = 5000;
 int commandStart;
+
+#include "pitches.h"
+int speakerOut = 11;
+int keyTone = NOTE_C4;
+int keyToneDuration = 100;
+String inString = "";
+
+char GetKey ()
+{
+  char key = keypad.getKey();
+  if(key)
+  {
+    tone(speakerOut, keyTone, keyToneDuration);
+  }
+  return key;
+}
+
+void SetPitch ()
+{
+  while (Serial.available() > 0)
+  {
+    int inChar = Serial.read();
+    if (isDigit(inChar))
+    {
+      // convert the incoming byte to a char and add it to the string:
+      inString += (char)inChar;
+    }
+    // if you get a newline, print the string, then the string's value:
+    if (inChar == '\n')
+    {
+      keyTone = inString.toInt();
+    }
+  }
+}
 
 void setup()
 {
   Serial.begin(9600);
+  while (!Serial)
+  {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
 }
 
 void loop()
 {
-  char key = keypad.getKey();
+  SetPitch();
+  char key = GetKey();
   
   if (key)
   {
@@ -52,36 +87,36 @@ void loop()
 
   if(key == commandChar)
   {
+    Serial.print("Command Entered: ");
     commandStart = millis();
-    bool invalidCommand = false;
-    bool correctCommand = false;
+    bool checkedCommand = false;
     int charIndex = 0;
+    
     while (millis() < commandStart + commandTimeout)
     {
-      if(!invalidCommand && !correctCommand)
+      key = GetKey();
+      if(charIndex < commandLength)
       {
-        key = keypad.getKey();
-
         if(key)
         {
-          Serial.print("Command Key ");
-          Serial.println(key);
-          if(key == changePinCommand[charIndex])
-          {
-            charIndex++;
-            if(charIndex == commandLength)
-            {
-              correctCommand = true;
-              Serial.print("Change Pin Command ");
-              Serial.println(changePinCommand);
-            }
-          }
-          else 
-          {
-            invalidCommand = true;
-          }
+          Serial.print(key);
+          enteredPin[charIndex] = key;
+          charIndex++;
+        }
+      }
+      else if(!checkedCommand)
+      {
+        checkedCommand = true;
+        Serial.println("");
+        Serial.print("Entered Command: ");
+        Serial.println(String(enteredPin));
+        if(enteredPin == changePinCommand)
+        {
+            checkedCommand = true;
+            Serial.println("Change Pin Command");
         }
       }
     }
+    
   }
 }
